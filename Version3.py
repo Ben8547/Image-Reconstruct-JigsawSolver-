4c4a008be729d07ff55f8e99756b356a363c7bf4
Ben8547 <102626149+Ben8547@users.noreply.github.com>
2026-02-20 17:38:59 -0500

    Update Version3.py
    Working on vectorizing; I want this to run as quickly as possible.

--- Version3.py
+++ Version3.py
@@ -3,6 +3,7 @@ The goal of this document is to further refine the genetic and annealing methods
 '''
 
 import numpy as np
+from numpy.random import random, randint, choice
 import matplotlib.pyplot as plt
 import cv2
 from time import sleep # for debug utility
@@ -71,6 +72,8 @@ else:
 
 grid = np.arange(0,64,1,dtype=np.uint8).reshape((8,8)) # the representation of the image; using uint8 because nothing is negative or bigger than 255 and thus using any other integer system would be wasteful
 
+tiles = np.array(tiles, dtype=object) # apparently you can make a list of dictionaries into an array - this makes indexing later much easier - this is a change from the previous version
+
 ''' Energy Function '''
 
 compatability = lambda x,y: np.mean((x-y)**2) # energy function
@@ -136,10 +139,19 @@ class simulation_grid:
             for j in range(1,self.grid_shape[1]): # skip first column
                 # we don't want to double count interactions so we first only compute the energies to the left and obove each point (skipping the topmost and leftmost row/column)
                 # then since the edges do not interact we can stop here since each interacting edge has been counted exactly once.
-                energy += self.gradient_interact_energy((i,j))
+                energy += self.gradient_interact_energy(self.simGrid,(i,j))
         return energy
     
-    def interaction_energy(self, grid_point:tuple) -> float:
+    def total_energy_grid(self,grid) -> float: # same as the above function except uses an arbitrary grid instead of self.simGrid; grid must have the same shape as self.simGrid 
+        energy = 0.
+        for i in range(1,self.grid_shape[0]): # skip firt row
+            for j in range(1,self.grid_shape[1]): # skip first column
+                # we don't want to double count interactions so we first only compute the energies to the left and obove each point (skipping the topmost and leftmost row/column)
+                # then since the edges do not interact we can stop here since each interacting edge has been counted exactly once.
+                energy += self.gradient_interact_energy(grid,(i,j))
+        return energy
+    
+    def interaction_energy(self, grid, grid_point:tuple) -> float:
         '''grid_point is an index of the 2D array self.grid
         This function computes the interaction energy at the top and left sides of a tile; we only compute these two to prevent overcounting as we iterate through the array
         We modify this from the frist version in that now we simply need to search the self.cached_energies - this should make the function much faster since we only search an array instead
@@ -148,14 +160,109 @@ class simulation_grid:
         row = grid_point[0]
         column = grid_point[1]
 
-        top_neighbor = self.tile_data[self.simGrid[row-1,column]]
-        left_neighbor = self.tile_data[self.simGrid[row,column-1]]
-        current = self.tile_data[self.simGrid[row,column]]
+        top_neighbor = self.tile_data[grid[row-1,column]]
+        left_neighbor = self.tile_data[grid[row,column-1]]
+        current = self.tile_data[grid[row,column]]
 
         return self.cached_energies[current,top_neighbor,0] + self.cached_energies[current,left_neighbor,1]
     
-    
+    def check_boundary(self, grid_points:np.ndarray) -> np.ndarray:
+            """
+            This function determines which sides of a specified grid_point are on the boundary of the grid.
+            The previous version could only process a single coordinate pair at a time, I have changed this in the following way:
+            now instead of inputting a tuple we input an Nx2 array where N is the number of points that we want to check.
+            We then do logic on the array.
+            Output is Nx4 array
+            """
+            rows = grid_points[:,0] # 1D array of all row coordinates
+            cols = grid_points[:,1] # 1D array of all column coordinates
+            shape = self.grid_shape
 
+            top_open = rows > 0 # True when not bordering the top of the array  
+            bottom_open = rows < self.grid_shape[0]-1
+            left_open = cols > 0
+            right_open = cols < self.grid_shape[1]-1
+            '''This should be far more efficient than the code from before'''
+
+            return np.vstack([top_open, left_open, bottom_open, right_open]).reshape((4,len(rows))).transpose() # this was a dictionary but not it is an Nx4 array where the second dimension takes the standard direction ordering thus far.
+    
+    def local_energy(self,grid_points:np.ndarray) -> float:
+        '''Finds the energy of a single grid tile. Note that this is different from the interaction energy function above as this function returns
+        the energy on each side and not just on the top and left sides
+        The function takes in an Nx2 array of coordinates for N points. It returns a length N array'''
+        rows = grid_points[:,0]
+        columns = grid_points[:,1]
+
+        boundaries = self.check_boundary(grid_points)
+        current = self.tile_data[grid[rows,columns]]
+
+
+        top_energy = self.cached_energies[self.tile_data[current,grid[row-1,column]],0] if boundaries[0] else 0. # only compute the energy on the top if there is a top neighbour
+        left_energy = self.cached_energies[self.tile_data[current,grid[row,column-1]],1] if boundaries[1] else 0.
+        bottom_energy = self.cached_energies[self.tile_data[current,grid[row+1,column]],2] if boundaries[2] else 0.
+        right_energy = self.cached_energies[self.tile_data[current,grid[row,column+1]],3] if boundaries[3] else 0.
+
+        return top_energy + left_energy + bottom_energy + right_energy # total energy of local interactions
+
+    
+    def markovStep(self, tempurature: float):
+        '''
+        Docstring for markovStep
+        Mutates the self grid in accordance with the metropolis algorithm
+        '''
+
+        mode = randint(0,4) # choose from several difference mutation options at random
+
+        if mode == 0 or mode == 3: # swap two points with a preference for swapping points with high local energies
+            ''' We are changing this from the previous version to bias choosing points with high local energies
+             We can use self.cached_energies to quickly lookup the local energy of a tile in O(1) time - just indexing an array at most four times
+             There is still some small chance to swap two random rows'''
+            if random() < 0.95: # with 95% chance sample some pieces and choose the one with the highest local energy to swap with another point with high local energy
+                # sample 20 pieces from the puzzle and compute their local energies
+            
+            else: # with 5% chance swap two random pieces
+                piece_rows = choice(list(range(self.grid_shape[0])),2,replace=False)
+                piece_cols = choice(list(range(self.grid_shape[1])),2,replace=False) # choosing the indicies for the points to swap
+
+                new_grid = np.copy(self.simGrid)
+                new_grid[piece_rows[0],piece_cols[0]] = self.simGrid[piece_rows[1],piece_cols[1]]
+                new_grid[piece_rows[1],piece_cols[1]] = self.simGrid[piece_rows[0],piece_cols[0]]
+
+            # compute the energy comtributions (only compute the local contributions)
+
+            #previous_contribution = 
+            #new_contribution = 
+                
+
+        elif mode == 1:
+            '''
+            Same as in the previous version, use np.roll to permute columns or rows 
+            The idea of this movement is that once we assemble chunck of related pieces, this function can move the chuncks into the correct absolute position.
+            '''
+            if random() < 0.5: # permute rows with 50% probability
+                new_grid = np.roll(self.simGrid, randint(0,self.grid_shape[0]),axis=0)
+            else: # permute columns
+                new_grid = np.roll(self.simGrid, randint(0,self.grid_shape[1]),axis=1)
+
+            #previous_contribution = self.energy
+            #new_contribution = self.total_energy_grid(new_grid)
+            # should just beable to recompute along the affected row or column
+
+        elif mode == 2:
+            ''' Here I try to move an entire block of tiles
+            For simplicity we choose a rectangle shape and swap it with another rectangle with the same dimensions
+            This allows us to keep pieces that are already matched together'''
+
+        previous_contribution = self.energy # I do want to add these into the indevidual blocks; but for testing purposes and while the grids are small enough, I'll keep them here and recompute the entire grid energy
+        new_contribution = self.total_energy_grid(new_grid)
+
+        energy_change = previous_contribution - new_contribution
+
+        boltzmann_factor = np.exp(-energy_change/tempurature)
+
+        if random() < boltzmann_factor: # always except whern previous >= new, sometimes accept an increase in the energy - dig out of local minima.
+            self.simGrid = new_grid
+            self.energy += energy_change
 
 
 image = simulation_grid(grid, tiles, cache_energies)
