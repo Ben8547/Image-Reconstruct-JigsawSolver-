00e79700992d5e998cc52a125980d4e4ead30ff8
Ben8547 <102626149+Ben8547@users.noreply.github.com>
2026-02-22 13:48:56 -0500

    reorganizaton

--- Version3.py
+++ Version3.py
@@ -8,113 +8,6 @@ import matplotlib.pyplot as plt
 import cv2
 from time import sleep # for debug utility
 
-'''
-Setup
-'''
-
-'''Deicde color or non-color'''
-
-color = True
-
-'''Load in the test file (permanent)'''
-
-file = "RainbowFlower_Puzzle.jpg"
-
-if color:
-    color_volume = cv2.imread(file, cv2.IMREAD_COLOR)
-    '''
-    This outputs a 3 dimensional array: (hight, width, 3)
-    We will need to store data differently taking this into account.
-    '''
-else:
-    gray_matrix = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    #print(gray_matrix)
-
-'''Chop up the image'''
-
-tiles = []
-
-if not color:
-    width = gray_matrix.shape[1] # horizontal distance - should be the shoter of the two
-    length = gray_matrix.shape[0]
-    tile_width = width//8
-    tile_length = length//8
-    #print(tile_length)
-
-    for i in range(8):
-        for j in range(8):
-            tiles.append({
-                0: gray_matrix[tile_length*i,tile_width*j:tile_width*(j+1)],
-                2: gray_matrix[tile_length*(i+1)-1,tile_width*j:tile_width*(j+1)],
-                1: gray_matrix[tile_length*i:tile_length*(i+1),tile_width*j],
-                3: gray_matrix[tile_length*i:tile_length*(i+1),tile_width*(j+1)-1],
-                "entire": gray_matrix[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1)] # need this last one to reconstruct the array later
-            })
-    del gray_matrix # no need to store a large matrix any longer than we need it. We only need the boarders anyway
-else:
-    width = color_volume.shape[1] # horizontal distance - should be the shoter of the two
-    length = color_volume.shape[0]
-    tile_width = width//8
-    tile_length = length//8
-    #print(tile_length)
-
-    for i in range(8):
-        for j in range(8):
-            tiles.append({
-                0: color_volume[tile_length*i,tile_width*j:tile_width*(j+1),:], # top
-                2: color_volume[tile_length*(i+1)-1,tile_width*j:tile_width*(j+1),:], # bottom
-                1: color_volume[tile_length*i:tile_length*(i+1),tile_width*j,:], # left
-                3: color_volume[tile_length*i:tile_length*(i+1),tile_width*(j+1)-1,:], # right
-                "entire": color_volume[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1),:] # need this last one to reconstruct the array later
-            }) # we use the wierd ordering so that we can use modular arithmatic to send top to bottom and left to right easily (and the reverse)
-
-    del color_volume # no need to store a large matrix any longer than we need it. We only need the boarders anyway
-
-grid = np.arange(0,64,1,dtype=np.uint8).reshape((8,8)) # the representation of the image; using uint8 because nothing is negative or bigger than 255 and thus using any other integer system would be wasteful
-
-
-tiles = np.array(tiles, dtype=object) # apparently you can make a list of dictionaries into an array - this makes indexing later much easier - this is a change from the previous version
-
-''' Energy Function '''
-
-compatability = lambda x,y: np.mean((x-y)**2) # energy function
-
-'''
-Cache every possible interaction energy
-We will use the following data structure to store this information. Recall that each tile has an index from 0 to 63. Thus we define an array that is
-64 x 64 x 4; We read the array as follows: define the list ['top', 'left', 'bottom', 'right'] so that we can correspond 0 to 'top', 1 to 'left' and so on.
-Then the index [4,45,2] means that we are searching for the energy of the interaction between tile 5's bottom face and 46's top face. Similarly, [56,7,1] means that
-we are looking up the energy between 57's left face and 8's right.
-
-We do need to account for the diagonal elemets such as [3,3,2]. I'll set these to np.inf since I never want the tile to interact favorably with itself.
-
-The compatability function can take vector inputs, but would condense them to a single value so I don't think that we can vectorize this; especially since we would need to
-look everything up in the dictionary. Thus we'll cache with a loop.
-
-We can also write to a file for easier lookup in the future if we run the same image multiple times in testing but adding this is not a highpriority.
-
-Note that if we define sigma(i) = (i+2)%4 then 0->2 (top->bottom),  1->3, 2->0 and 3->1 exactly as desired to get the opposites; thus we don't even need the conversion dictionary that I
-was using in my previous versions.
-'''
-
-cache_energies = np.zeros((64,64,4),dtype=float)
-
-for i in range(64):
-    for j in range(64):
-        for d_i in range(4):
-            if i == j: # diagonal elements are set to infinite since they can never happen anyway
-                cache_energies[i,j,d_i] = np.inf
-            else:
-                cache_energies[i,j,d_i] = compatability( tiles[i][d_i], tiles[j][(d_i + 2) % 4] ) # although tiles could be indexed with an array, I think compatability would average over everything so We'll have to settle for the loop
-
-# this is actually suprisingly quick to compute though probably won't scale well for large puzzles. Luckily we only care about 64x64 right now.
-# in the current case it might actually take longer to open a read a file than just recompute all of the energies
-print("cached tile energies")
-
-'''
-Compute Best-Buddies
-'''
-
 
 '''
 "Smart" Annealing
@@ -235,7 +128,7 @@ class simulation_grid:
         '''Ideally at some point we make each move dependant on the tempurature schedule; we prefer larger moves at low T so that we can cement absolute positions
         though we might have to implement the genetic algorithm again to get the absolute positions, the idea in that method was decent'''
 
-        if mode == 0 or mode == 3: # swap two points with a preference for swapping points with high local energies
+        if mode == 0 or mode == 3 or mode == 2: # swap two points with a preference for swapping points with high local energies
             ''' We are changing this from the previous version to bias choosing points with high local energies
              We can use self.cached_energies to quickly lookup the local energy of a tile in O(1) time - just indexing an array at most four times
              There is still some small chance to swap two random rows'''
@@ -352,33 +245,143 @@ class simulation_grid:
             i += 1
 
 
+if __name__ == "__main__": # so that we can just import the class if desired
 
-simulation = simulation_grid(grid, tiles, cache_energies)
-simulation.anneal()
+    '''
+    Setup
+    '''
 
-'''Now that we have the ordered array, all that remains is to put the grayscale map back together.'''
+    '''Deicde color or non-color'''
 
-if color:
-    resotred_page = np.zeros((length,width,3))
+    color = True
 
-    for i in range(simulation.grid_shape[0]):
-        for j in range(simulation.grid_shape[1]):
-            dict_index = simulation.simGrid[i,j]
-            resotred_page[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1),:] = simulation.tile_data[dict_index]["entire"]
+    '''Load in the test file (permanent)'''
 
-    resotred_page = resotred_page.astype(np.uint8) # jpg can only handle this resolution anyway
-    cv2.imwrite(f"annealing-color.jpg", resotred_page)
-else:
-    resotred_page = np.zeros((length,width))
+    file = "RainbowFlower_Puzzle.jpg"
 
-    for i in range(simulation.grid_shape[0]):
-        for j in range(simulation.grid_shape[1]):
-            dict_index = simulation.simGrid[i,j]
-            resotred_page[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1)] = simulation.tile_data[dict_index]["entire"]
-    resotred_page = resotred_page.astype(np.uint8)
-    cv2.imwrite(f"annealing-grayscale.jpg", resotred_page)
+    if color:
+        color_volume = cv2.imread("Inputs/"+file, cv2.IMREAD_COLOR)
+        '''
+        This outputs a 3 dimensional array: (hight, width, 3)
+        We will need to store data differently taking this into account.
+        '''
+    else:
+        gray_matrix = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        #print(gray_matrix)
 
-print(f"Final energy {simulation.energy}")
+    '''Chop up the image'''
 
-plt.imshow(resotred_page)
-plt.show()
+    tiles = []
+
+    if not color:
+        width = gray_matrix.shape[1] # horizontal distance - should be the shoter of the two
+        length = gray_matrix.shape[0]
+        tile_width = width//8
+        tile_length = length//8
+        #print(tile_length)
+
+        for i in range(8):
+            for j in range(8):
+                tiles.append({
+                    0: gray_matrix[tile_length*i,tile_width*j:tile_width*(j+1)],
+                    2: gray_matrix[tile_length*(i+1)-1,tile_width*j:tile_width*(j+1)],
+                    1: gray_matrix[tile_length*i:tile_length*(i+1),tile_width*j],
+                    3: gray_matrix[tile_length*i:tile_length*(i+1),tile_width*(j+1)-1],
+                    "entire": gray_matrix[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1)] # need this last one to reconstruct the array later
+                })
+        del gray_matrix # no need to store a large matrix any longer than we need it. We only need the boarders anyway
+    else:
+        width = color_volume.shape[1] # horizontal distance - should be the shoter of the two
+        length = color_volume.shape[0]
+        tile_width = width//8
+        tile_length = length//8
+        #print(tile_length)
+
+        for i in range(8):
+            for j in range(8):
+                tiles.append({
+                    0: color_volume[tile_length*i,tile_width*j:tile_width*(j+1),:], # top
+                    2: color_volume[tile_length*(i+1)-1,tile_width*j:tile_width*(j+1),:], # bottom
+                    1: color_volume[tile_length*i:tile_length*(i+1),tile_width*j,:], # left
+                    3: color_volume[tile_length*i:tile_length*(i+1),tile_width*(j+1)-1,:], # right
+                    "entire": color_volume[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1),:] # need this last one to reconstruct the array later
+                }) # we use the wierd ordering so that we can use modular arithmatic to send top to bottom and left to right easily (and the reverse)
+
+        del color_volume # no need to store a large matrix any longer than we need it. We only need the boarders anyway
+
+    grid = np.arange(0,64,1,dtype=np.uint8).reshape((8,8)) # the representation of the image; using uint8 because nothing is negative or bigger than 255 and thus using any other integer system would be wasteful
+
+
+    tiles = np.array(tiles, dtype=object) # apparently you can make a list of dictionaries into an array - this makes indexing later much easier - this is a change from the previous version
+
+    ''' Energy Function '''
+
+    compatability = lambda x,y: np.mean((x-y)**2) # energy function
+
+    '''
+    Cache every possible interaction energy
+    We will use the following data structure to store this information. Recall that each tile has an index from 0 to 63. Thus we define an array that is
+    64 x 64 x 4; We read the array as follows: define the list ['top', 'left', 'bottom', 'right'] so that we can correspond 0 to 'top', 1 to 'left' and so on.
+    Then the index [4,45,2] means that we are searching for the energy of the interaction between tile 5's bottom face and 46's top face. Similarly, [56,7,1] means that
+    we are looking up the energy between 57's left face and 8's right.
+
+    We do need to account for the diagonal elemets such as [3,3,2]. I'll set these to np.inf since I never want the tile to interact favorably with itself.
+
+    The compatability function can take vector inputs, but would condense them to a single value so I don't think that we can vectorize this; especially since we would need to
+    look everything up in the dictionary. Thus we'll cache with a loop.
+
+    We can also write to a file for easier lookup in the future if we run the same image multiple times in testing but adding this is not a highpriority.
+
+    Note that if we define sigma(i) = (i+2)%4 then 0->2 (top->bottom),  1->3, 2->0 and 3->1 exactly as desired to get the opposites; thus we don't even need the conversion dictionary that I
+    was using in my previous versions.
+    '''
+
+    cache_energies = np.zeros((64,64,4),dtype=float)
+
+    for i in range(64):
+        for j in range(64):
+            for d_i in range(4):
+                if i == j: # diagonal elements are set to infinite since they can never happen anyway
+                    cache_energies[i,j,d_i] = np.inf
+                else:
+                    cache_energies[i,j,d_i] = compatability( tiles[i][d_i], tiles[j][(d_i + 2) % 4] ) # although tiles could be indexed with an array, I think compatability would average over everything so We'll have to settle for the loop
+
+    # this is actually suprisingly quick to compute though probably won't scale well for large puzzles. Luckily we only care about 64x64 right now.
+    # in the current case it might actually take longer to open a read a file than just recompute all of the energies
+    print("cached tile energies")
+
+    '''
+    Compute Best-Buddies
+    '''
+
+
+    simulation = simulation_grid(grid, tiles, cache_energies)
+    print(f"Initial Energy: {simulation.energy}")
+    simulation.anneal()
+
+    '''Now that we have the ordered array, all that remains is to put the grayscale map back together.'''
+
+    if color:
+        resotred_page = np.zeros((length,width,3))
+
+        for i in range(simulation.grid_shape[0]):
+            for j in range(simulation.grid_shape[1]):
+                dict_index = simulation.simGrid[i,j]
+                resotred_page[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1),:] = simulation.tile_data[dict_index]["entire"]
+
+        resotred_page = resotred_page.astype(np.uint8) # jpg can only handle this resolution anyway
+        cv2.imwrite("Outputs/"+f"annealing-color.jpg", resotred_page)
+    else:
+        resotred_page = np.zeros((length,width))
+
+        for i in range(simulation.grid_shape[0]):
+            for j in range(simulation.grid_shape[1]):
+                dict_index = simulation.simGrid[i,j]
+                resotred_page[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1)] = simulation.tile_data[dict_index]["entire"]
+        resotred_page = resotred_page.astype(np.uint8)
+        cv2.imwrite("Outputs/"+f"annealing-grayscale.jpg", resotred_page)
+
+    print(f"Final energy {simulation.energy}")
+
+    plt.imshow(resotred_page)
+    plt.show()
