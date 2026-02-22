a08ecbe227690458234e44504b15d9a0321b1dac
Ben8547 <102626149+Ben8547@users.noreply.github.com>
2026-02-22 00:22:50 -0500

    Update Version3.py
    Mostly complete - doing a test run

--- Version3.py
+++ Version3.py
@@ -14,7 +14,7 @@ Setup
 
 '''Deicde color or non-color'''
 
-color = True
+color = False
 
 '''Load in the test file (permanent)'''
 
@@ -122,18 +122,21 @@ Here I try to adjust the annealing in a few ways;
 a) replace my global energy recalculations in each markov step with local ones which should save compute time.
 b) Add more annealing movement options that take into account the global energy information and try to move compatable tiles near each other.
 c) Also try adding some more global rearangements to get blocks of aggregated tiles into the correct absolute position.
-d) Create a more precise temperature schedule since the current geometrix cooling seems to waste a lot of time spuddling on high temperatures.
+d) Create a more precise temperature schedule since the current geometric cooling seems to waste a lot of time spuddling on high temperatures.
 '''
 
 class simulation_grid:
     def __init__(self, grid, dict_list, cached_energies):
         self.simGrid = grid
-        self.paddedGrid = self.pad(grid) # pad the simGrid; important to facilitate vectorization of certain methods though this does warent more careful indexing.
         self.tile_data = dict_list
         self.grid_shape = self.simGrid.shape # note that this is the shape of the non-padded
         self.cached_energies = cached_energies # as far as I know, this just points to the original array so we are not actually making a copy of the large energy array
         self.energy = self.total_energy() # set the total energy on creating the class
         # for all intents and purposes recall that 0 = top, 1 = left, 2 = bottom, 3 = right which is quite a different ordering than the previous verison.
+        #annealing constants:
+        self.T0 = 10.
+        self.Tf = 0.5
+        self.geometric_rate = 0.999
     
     def total_energy(self) -> float:
         '''This should be much faster since I only use no explicit python loops instead of the 2 from the previous version
@@ -185,11 +188,11 @@ class simulation_grid:
         shape = array.shape
         # add top and bottom
         array = array.astype(np.int8) # we need to switch from uint8 since we are adding -1s.
-        array = np.vstack([-np.ones((1,shape[1])), array])
-        array = np.vstack([array, -np.ones((1,shape[1]))])
+        array = np.vstack([-np.ones((1,shape[1]),dtype=np.int8), array])
+        array = np.vstack([array, -np.ones((1,shape[1]),dtype=np.int8)])
         # add left/right
-        array = np.hstack([-np.ones((shape[0]+2,1)), array])
-        array = np.hstack([array, -np.ones((shape[0]+2,1))])
+        array = np.hstack([-np.ones((shape[0]+2,1),dtype=np.int8), array])
+        array = np.hstack([array, -np.ones((shape[0]+2,1),dtype=np.int8)])
         return array
     
     def local_energy(self,grid,grid_points:np.ndarray) -> float:
@@ -197,9 +200,10 @@ class simulation_grid:
         the energy on each side and not just on the top and left sides
         The function takes in an Nx2 array of coordinates for N points. It returns a length N array
         We use the padded array here to allow indexing withough if statements. We remove the -1s by musltiplying them by False
-        Note that the input grid_points should be for the unpadded array - we convert to the padded indicies herein'''
+        Note that the input grid_points should be for the unpadded array - we convert to the padded indicies herein, the input grid should also be unpadded'''
         rows = grid_points[:,0]+1
         columns = grid_points[:,1]+1
+        grid = self.pad(grid)
         # adding one account for the extra later of padding
 
         boundaries = self.check_boundary(grid_points) # Nx4 array
@@ -225,34 +229,38 @@ class simulation_grid:
         '''Ideally at some point we make each move dependant on the tempurature schedule; we prefer larger moves at low T so that we can cement absolute positions
         though we might have to implement the genetic algorithm again to get the absolute positions, the idea in that method was decent'''
 
-        if mode == 0 or mode == 3: # swap two points with a preference for swapping points with high local energies
+        if mode == 0 or mode == 3 or mode == 2: # swap two points with a preference for swapping points with high local energies
             ''' We are changing this from the previous version to bias choosing points with high local energies
              We can use self.cached_energies to quickly lookup the local energy of a tile in O(1) time - just indexing an array at most four times
              There is still some small chance to swap two random rows'''
-            Falied = False
+            Failed = False
             if random() < 0.95: # with 95% chance sample some pieces and choose the one with the highest local energy to swap with another point with high local energy
                 # sample 20 pieces from the puzzle and compute their local energies
-                samples = choice(self.simGrid,20,replace=False)
-                worst_sample = samples[np.argmax(self.local_energy(self.simGrid,s) for s in samples)] # Gets sampled with the highest local energy; could probably vectotize but would need to rewrite the local_energy function
+                sample_cols = randint(0,self.grid_shape[1],(20,1),dtype=np.uint8) # we don't turn off replacement, but the chance of duplicates is relatively low so it won't matter
+                sample_rows = randint(0,self.grid_shape[0],(20,1),dtype=np.uint8)
+                samples = np.hstack([sample_rows,sample_cols],dtype=np.uint8)
+                worst_sample = samples[np.argmax(self.local_energy(self.simGrid,samples))] # Gets sample with the highest local energy; this return an index of a 2d array
+                worst_sample = (worst_sample[0],worst_sample[1]) # it is important that this is a tuple, if it is a list or an array, then numpy indexing treats it as two separate indicies to be looked up
                 # now we choose a direction and find its most optimal position in that direction and swap with the tile current in that position.
                 d = randint(0,4)
-                best_partner = np.argmin(self.cached_energies[worst_sample,:,d]) # should return the index of the best partner in that direction
+                best_partner = np.argmin(self.cached_energies[self.simGrid[worst_sample],:,d]) # should return the index of the best partner in that direction
                 # make the swap:
-                piece_location = np.argwhere(self.simGrid == worst_sample)
-                partner_location = np.argwhere( self.simGrid == best_partner )
-                partner_location[0] += (d - 1) * ( d%2 == 0 )  # add to the indicies depending on direction. If d=0 we change by -1, if d = 2 we change by +1 else by 0. Thus we can take d-1 but only if d is even
-                partner_location[1] += (d - 2) * (d%2 == 1) # need -1 if d = 1 and +1 if d = 3 else 0. d-2 if odd should work; I'm am quite proud of myself for the branchless logic today.
+                partner_location = np.argwhere( self.simGrid == best_partner )[0] # returns the matching piece, not the location to move the piece to; returns a 1x2 array which is why we must extract [0]
+                partner_location[0] += -(d - 1) * ( d%2 == 0 )  # add to the indicies depending on direction. If d=0 we change by +1, if d = 2 we change by -1 else by 0. Thus we can take d-1 but only if d is even. 
+                # We invert from the standard because we looked it up so that worst_sample was paired at it's d face, so we actaully need the complementary direction on the partner.
+                partner_location[1] += -(d - 2) * (d%2 == 1) # need +1 if d = 1 and -1 if d = 3 else 0. d-2 if odd should work; I'm am quite proud of myself for the branchless logic today.
+                partner_location = (partner_location[0],partner_location[1])
                 # we still have not accounted for the piece being on the boarder
                 if partner_location[0] < 0 or partner_location[0] > self.grid_shape[0]-1 or partner_location[1] < 0 or partner_location[1] > self.grid_shape[1]-1:
-                    Falied = True # Then the swap is not valid so we move to a random swap instead
+                    Failed = True # Then the swap is not valid so we move to a random swap instead
                 else: # then we proceed with the swap
                     new_grid = np.copy(self.simGrid)
-                    new_grid[partner_location] = self.simGrid[piece_location]
-                    new_grid[piece_location] = self.simGrid[partner_location]
+                    new_grid[partner_location] = self.simGrid[worst_sample]
+                    new_grid[worst_sample] = self.simGrid[partner_location]
             else: # with 5% chance swap two random pieces (more than 5% once you account for the failed swaps above)
-                Falied = True
+                Failed = True
 
-            if Falied: # make a random swap
+            if Failed: # make a random swap
                 piece_rows = choice(list(range(self.grid_shape[0])),2,replace=False)
                 piece_cols = choice(list(range(self.grid_shape[1])),2,replace=False) # choosing the indicies for the points to swap
 
@@ -296,5 +304,54 @@ class simulation_grid:
             self.simGrid = new_grid
             self.energy += energy_change
 
+    def cooling_schedule_optimal(self,T_0, k):
+        '''Note that to reach a final temperature of T_f we
+        need to run the algorithm for exp(T_0/T_f) and since T_f is small and T_0 large, this is usually a very large number.'''
+        # only call if k > 1
+        # The proof of Hajek, 1998 gaurentees that this cooling schedule leads to converge if T_0 > largest minimal depth of an element of the solution space
+        # because of the logarithmic scaling, this scedule takes a long time to resolve so I may change it later.
+        return T_0/np.log(k)
+    
+    def cooling_schedule_geometric(self,T_0,rate,k):
+        return T_0 * (rate**k)
 
-image = simulation_grid(grid, tiles, cache_energies)
+
+    def anneal(self):
+        '''This method runs the annealing process by iternating through markov with a tempurature schedule'''
+        T = self.T0
+        i = 2
+        while T > self.Tf: 
+            self.markovStep(T)
+            print(f"Energy: {self.energy}, Temperature: {T}") # to track progress
+            #T = self.cooling_schedule_geometric(self.T0,self.geometric_rate,i) #update the tempurature
+            T = self.cooling_schedule_optimal(self.T0,i) #update the tempurature
+            i += 1
+
+
+
+simulation = simulation_grid(grid, tiles, cache_energies)
+simulation.anneal()
+
+'''Now that we have the ordered array, all that remains is to put the grayscale map back together.'''
+
+if color:
+    resotred_page = np.zeros((length,width,3))
+
+    for i in range(simulation.grid_shape[0]):
+        for j in range(simulation.grid_shape[1]):
+            dict_index = simulation.simGrid[i,j]
+            resotred_page[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1),:] = simulation.tile_data[dict_index]["entire"]
+
+    resotred_page = resotred_page.astype(np.uint8) # jpg can only handle this resolution anyway
+    cv2.imwrite(f"annealing-color.jpg", resotred_page)
+else:
+    resotred_page = np.zeros((length,width))
+
+    for i in range(simulation.grid_shape[0]):
+        for j in range(simulation.grid_shape[1]):
+            dict_index = simulation.simGrid[i,j]
+            resotred_page[tile_length*i:tile_length*(i+1),tile_width*j:tile_width*(j+1)] = simulation.tile_data[dict_index]["entire"]
+        cv2.imwrite(f"annealing-grayscale.jpg", resotred_page)
+
+plt.imshow(resotred_page)
+plt.show()
